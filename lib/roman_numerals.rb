c367ac43fecdbff4d19965c6fc1f2e61e717da10
Patrick Harris <patricknpharris@gmail.com>
2018-08-30 11:54:27 +0100

    refactored into class and reproduced simple test

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,28 +1,38 @@
-def roman_numerals(number)
-  result = ''
-  while number > 0
+# class for converting arabic number provided to roman numeral
+class RomanNumber
+  def initialize(number)
+    @result = ''
+    @number = number
+  end
+
+  def roman_numerals
+    @result = ''
+    match_numeral while @number > 0
+    @result
+  end
+
+  def match_numeral
     $roman_array.each do |element|
-      if number - element[0] >= 0
-        result.concat(element[1])
-        number -= element[0]
+      if @number - element[0] >= 0
+        @result.concat(element[1])
+        @number -= element[0]
       end
     end
   end
-  return result
-end
 
-$roman_array = [
-  [1000,'M'],
-  [900,'CM'],
-  [500,'D'],
-  [400,'CD'],
-  [100,'C'],
-  [90,'XC'],
-  [50,'L'],
-  [40,'XL'],
-  [10,'X'],
-  [9,'IX'],
-  [5,'V'],
-  [4,'IV'],
-  [1,'I']
-]
+  $roman_array = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ]
+end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,18 +1,8 @@
-require 'roman_numerals'
+require 'roman_numerals.rb'
 
-describe 'roman_numerals' do
-  it 'returns single roman numeral when passed 1, 5, 10, 50, 100, 500, 1000' do
-    expect(roman_numerals(1)).to eq 'I'
-    expect(roman_numerals(5)).to eq 'V'
-    expect(roman_numerals(10)).to eq 'X'
-    expect(roman_numerals(50)).to eq 'L'
-    expect(roman_numerals(100)).to eq 'C'
-    expect(roman_numerals(500)).to eq 'D'
-    expect(roman_numerals(1000)).to eq 'M'
-  end
-
-  it  "return double roman numeral when passed 2, 6" do
-    expect(roman_numerals(2)).to eq 'II'
-    expect(roman_numerals(152)).to eq 'CLII'
+describe RomanNumber do
+  it 'returns V when passed 5' do
+    number = RomanNumber.new(5)
+    expect(number.roman_numerals).to eq 'V'
   end
 end
